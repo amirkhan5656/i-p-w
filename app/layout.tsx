d2a656269e4a6dc3d07ada8992bfe7a60f8350ba
Amir Khan <amirqureshi590590@gmail.com>
2025-05-20 23:22:54 -0700

    second add navbar remove bug commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar";
+import Navbar from "../components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
