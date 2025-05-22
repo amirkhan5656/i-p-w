1b7f6cea91d7fb917928876b914a8c574a6fefc2
Amir Khan <amirqureshi590590@gmail.com>
2025-05-21 23:55:44 -0700

    remove Image from services

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
